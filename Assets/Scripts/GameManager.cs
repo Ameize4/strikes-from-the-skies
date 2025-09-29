356743d1fb3d8561b3150c6747fffac480c53e9f
Ameize4 <ameize4@gmail.com>
2025-09-29 14:54:39 +0300

    Add CameraShake and LightmapSwap (#3)
    * Add perlin noise to camera as CameraShake script
    * Add lightmap swapper

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Linq;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Yarn.Unity;
 
 namespace DefaultNamespace
@@ -7,19 +10,40 @@ namespace DefaultNamespace
     public class GameManager : MonoBehaviour
     {
         public static GameManager Instance;
+
+        [FormerlySerializedAs("camera")] [SerializeField] private Transform followPoint;
         
+        [Space]
         [SerializeField] DialogueRunner dialogueRunner;
         [SerializeField] DialogueReference dialogue;
+
+        [Serializable]
+        public struct LightingInfo
+        {
+            public Texture2D Color;
+            // public Texture2D ShadowMask;
+        }
+        [Space]
+        [SerializeField] private LightingInfo[] _lightingInfos;
 
         [Space]
         public Map.Grid grid;
 
         public Map.Chapters chapters;
         [SerializeField] private int currentChapterIdx = 0;
+        
+        [Space]
+        [SerializeField] private CameraShake.CameraShakeProperties cameraShakeProperties;
+        private CameraShake _cameraShake;
 
+        
         public AudioClip enemyAudioClip;
         
         public Map.EnemyData[] enemiesData;
+
+        // int values of KeyCode Enum of keyboard numbers
+        public int alphaKeyCode1 = 49;
+        public int alphaKeyCode9 = 57;
 
         private void Awake()
         {
@@ -28,15 +52,49 @@ namespace DefaultNamespace
 
         private void Start()
         {
-            dialogueRunner.StartDialogue(dialogue.nodeName);
+            // dialogueRunner.StartDialogue(dialogue.nodeName);
+            
+            CinemachineCore.CameraActivatedEvent.AddListener(CameraChangedListener);
+
+            _cameraShake = new CameraShake(cameraShakeProperties);
+            _cameraShake.SetNewTarget(followPoint);
         }
 
+        private void CameraChangedListener(ICinemachineCamera.ActivationEventParams arg0)
+        {
+            var IncomingCamera = (CinemachineCamera)arg0.IncomingCamera;
+            var newTrackingObject = IncomingCamera.Target.TrackingTarget;
+            if (newTrackingObject == null) newTrackingObject = IncomingCamera.transform;
+
+            _cameraShake.SetNewTarget(newTrackingObject);
+        }
+
+        
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.P))
             {
                 grid.BeginEnemyWave(enemiesData);
             }
+            if (Input.GetKey(KeyCode.O))
+            {
+                _cameraShake.SetTrauma(1f);
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                for (var index = 0; index < _lightingInfos.Length; index++)
+                {
+                    var info = _lightingInfos[index];
+                    KeyCode tempKeyCode = (KeyCode)(alphaKeyCode1+index);
+                    if (Input.GetKeyDown(tempKeyCode))
+                    {
+                        SetLightmaps(info);
+                    }
+                }
+            }
+            
+            _cameraShake.Process();
         }
 
         [YarnCommand("SpawnWave")]
@@ -84,6 +142,14 @@ namespace DefaultNamespace
             {
                 dialogueRunner.StartDialogue(chapters.chapters[currentChapterIdx].dialogueAfterWave);
             }
+        }
+        
+        public void SetLightmaps(LightingInfo info)
+        {
+            LightmapData data = new LightmapData();
+            data.lightmapColor = info.Color;
+            // data.shadowMask = info.ShadowMask;
+            LightmapSettings.lightmaps = new LightmapData[] { data };
         }
     }
 }
